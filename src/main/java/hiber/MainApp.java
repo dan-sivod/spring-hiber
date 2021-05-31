f81d6f3e53a6f81de707fc513d2eb33e3cea8efd
89104775491Ds <dan-sivod@mail.ru>
2021-06-01 00:58:00 +0300

    add Car,CarDao,CarService

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -1,7 +1,9 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
+import hiber.service.CarService;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -14,11 +16,19 @@ public class MainApp {
             new AnnotationConfigApplicationContext(AppConfig.class);
 
       UserService userService = context.getBean(UserService.class);
+      CarService carService = context.getBean(CarService.class);
 
       userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
+      carService.add(new Car("Toyota",1));
+
       userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
+      carService.add(new Car("Toyota",2));
+
       userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
+      carService.add(new Car("Toyota",3));
+
       userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      carService.add(new Car("Toyota",4));
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -26,6 +36,7 @@ public class MainApp {
          System.out.println("First Name = "+user.getFirstName());
          System.out.println("Last Name = "+user.getLastName());
          System.out.println("Email = "+user.getEmail());
+         System.out.println("Car = " );
          System.out.println();
       }
 
---
+++ src/main/java/hiber/model/Car.java
@@ -0,0 +1,86 @@
+package hiber.model;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @OneToOne(mappedBy = "car",cascade = CascadeType.ALL)
+    private User user;
+
+    @Column(name = "model")
+    private String model;
+
+    @Column(name = "series")
+    private int series;
+
+    public Car() {
+    }
+
+    public Car(String model, int series) {
+        this.model = model;
+        this.series = series;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "id=" + id +
+                ", user=" + user +
+                ", model='" + model + '\'' +
+                ", series=" + series +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return series == car.series && id.equals(car.id) && user.equals(car.user) && model.equals(car.model);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, user, model, series);
+    }
+}
